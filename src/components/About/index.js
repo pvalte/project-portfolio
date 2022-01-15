7f3dd4880f8af9cabdef3791ace6eed188150d8d
Paige A <pvaltemare@gmail.com>
2022-01-15 11:45:10 -0600

    style about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,14 +3,14 @@ import profilePic from '../../assets/avatar.JPG';
 
 function About() {
     return (
-        <section>
+        <section className='about'>
             <h1 id="about">About Me</h1>
             <div className="flex-row">
-                <img src={profilePic} style={{ width: "100px" }} alt="avatar" />
+                <img src={profilePic} alt="avatar" />
                 <p>
                     Hi! My name is Paige Altemare and I am a Coder living in Chicago, Illinois. I graduated from Case Western Reserve University (CWRU) in 2016 with a BS in Chemical Engineering. I became a Systems Engineer in January 2021, working in primarily C# and JavaScript.
-                </p>
-                <p>
+                    <br></br>
+                    <br></br>
                     I am taking a Coding Bootcamp through CWRU to become certified in full stack coding. I believe that there need to be more women in technology and hope that I can help break down barriers for other women entering the field.
                 </p>
             </div>
